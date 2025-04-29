4a7a93fb045287dceb425f56ef0db899c3676447
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-04-29 17:38:48 -0400

    examples: add lz.n example

---
+++ examples/lazy/flake.nix
@@ -0,0 +1,56 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    mnw.url = "../../.";
+    # If you're actually using this, change your input to this:
+    # mnw.url = "github:Gerg-L/mnw";
+  };
+  outputs =
+    {
+      nixpkgs,
+      mnw,
+      self,
+      ...
+    }:
+    {
+      packages.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          lazify = plugins: map (plugin: plugin // { optional = true; }) plugins;
+
+          nonLazyPlugins = [
+            pkgs.vimPlugins.lz-n
+          ];
+
+          # Anything that you're loading lazily should be put here
+          lazyPlugins = lazify [
+            pkgs.vimPlugins.oil-nvim
+          ];
+        in
+        {
+          default = mnw.lib.wrap pkgs {
+            neovim = pkgs.neovim-unwrapped;
+
+            # all files in the `lua/lazy` folder are now autoloaded, so no need
+            # for an init.lua in there
+            initLua = ''
+              require('myconfig')
+              require('lz.n').load('lazy')
+            '';
+
+            devExcludedPlugins = [
+              ./nvim
+            ];
+            devPluginPaths = [
+              # This normally should be a absolute path
+              # here it'll only work from this directory
+              "./nvim"
+            ];
+
+            plugins = nonLazyPlugins ++ lazyPlugins;
+          };
+
+          dev = self.packages.x86_64-linux.default.devMode;
+        };
+    };
+}
