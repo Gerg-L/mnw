bd67b6ca5330f7edf0b34d64ffb6290df061dc66
Gerg-L <GregLeyda@proton.me>
2026-06-06 14:55:59 -0400

    Revert "fix: use string interpolation when required"
    This reverts commit 0902144a302ff3b43fca93cd62575a626b3e5226.
    
    Revert "fix: don't recopy paths to store"
    
    This reverts commit c1c0b544bfabe6669b5a6a0383ccb475fe60258b.

--- wrapper.nix
+++ wrapper.nix
@@ -30,14 +30,12 @@ lib.makeOverridable (
   }@mnwWrapperArgs:
   let
 
-    toStringIfNeeded = x: if !lib.isStorePath x then "${x}" else toString x;
-
     pluginsToListOfStrings = lib.mapAttrsToList (
       n: v:
       if lib.isStringLike v then
-        toStringIfNeeded v
+        "${v}"
       else if v ? src && lib.isStringLike v.src then
-        toStringIfNeeded v.src
+        "${v.src}"
       else
         throw "mnw: plugin '${n}' cannot be coerced to string, ensure it has a 'outPath' or 'src'"
     );
@@ -78,10 +76,10 @@ lib.makeOverridable (
         luaEnv = neovim.lua.withPackages extraLuaPackages;
         inherit (neovim.lua.pkgs) luaLib;
 
-        sourceLua = lib.concatMapStringsSep "\n" (x: "dofile('${toStringIfNeeded x}')") (
+        sourceLua = lib.concatMapStringsSep "\n" (x: "dofile('${x}')") (
           (lib.optional (initLua != "") (writeText "init.lua" initLua)) ++ luaFiles
         );
-        sourceVimL = lib.concatMapStringsSep "\n" (x: "vim.cmd('source ${toStringIfNeeded x}')") (
+        sourceVimL = lib.concatMapStringsSep "\n" (x: "vim.cmd('source ${x}')") (
           (lib.optional (initViml != "") (writeText "init.vim" initViml)) ++ vimlFiles
         );
       in
